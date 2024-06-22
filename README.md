3bd11fc1b51f86c51413856817b4e3a6e9ab3e6d
Matthew McAllister <matthew.mcallister.0@gmail.com>
2024-06-22 14:30:45 -0700

    Add skeleton

---
+++ README.md
@@ -0,0 +1,9 @@
+# SDL OpenGL demo
+
+1. Install clang for windows
+2. Install the Ninja build system
+3. Install vcpkg and follow the "Getting started" tutorial: https://learn.microsoft.com/en-us/vcpkg/get_started/get-started?pivots=shell-cmd
+4. Make sure everything is on your path as needed
+5. (Optional?) Run `vcpkg install`
+6. (Optional) Install the "CMake Tools" VSCode extension and run the "Set preset" command to use the "Clang Windows Debug" preset. Alternatively, you can just run CMake with `--preset=clang-windows-debug`.
+7. Configure, build, run.
---
+++ src/main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <exception>
+
+#include <glad/glad.h>
+#include <SDL.h>
+
+using std::cerr;
+using std::cout;
+using std::endl;
+
+class UserExit : std::exception {};
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};  
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+void main_loop() {
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        throw std::runtime_error{"Init SDL failed"};
+    }
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    auto window = SDL_CreateWindow(
+        "Example",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (!window) {
+        throw std::runtime_error{"Window create failed"};
+    }
+    auto context = SDL_GL_CreateContext(window);
+    if (!context) {
+        throw std::runtime_error{"Create GL context failed"};
+    }
+
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+    if (SDL_GL_SetSwapInterval(1) < 0) {
+        cerr << "Failed to enable vsync" << endl;
+    }
+
+    SDL_StartTextInput();
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);   
+    glDeleteShader(fragmentShader);
+
+    while (true) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                throw UserExit();
+            }
+        }
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        SDL_GL_SwapWindow(window);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    try {
+        main_loop();
+    } catch (UserExit e) {
+        return 0;
+    } catch (std::runtime_error e) {
+        cout << e.what() << endl;
+        return 1;
+    }
+    return 0;
+}
